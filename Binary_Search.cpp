c9fe0d5a43aab99b97e3eb66284ad0badc82cc2e
Nguyễn Hoàng Phúc <115894727+LoylP@users.noreply.github.com>
2023-03-06 22:40:45 +0700

    Add files via upload

---
+++ Binary_Search.cpp
@@ -0,0 +1,28 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+const int MaxN = 1 + 1e6;
+
+int n, k, a[MaxN];
+
+bool check(ll days) {
+    ll products = 0;
+    for (int i = 0; i < n; ++i) products += days / a[i];
+    return products >= k;
+}
+
+int main() {
+    cin >> n >> k;
+    for (int i = 0; i < n; ++i) cin >> a[i];
+    ll l = 0, r = 1e18;
+    while (l < r) {
+        ll tg = (l + r) / 2;
+        if (check(tg)) r = tg;
+        else l = tg + 1;
+    }
+    cout << l << endl;
+
+    return 0;
+}
